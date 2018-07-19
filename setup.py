3d9234b690fc729bc21c1a27e86dc337c362f4c3
Quique Piña <epina@apsl.net>
2018-07-19 12:46:27 +0200

    metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="Memento",
     version="0.1",
+    author="Enrique Piña Monserrat",
+    author_email="lord.feratum@gmail.com",
     description="Create checkpoints and rollback for your data",
+    url="https://github.com/LordFeratum/memento",
+    download_url="https://github.com/LordFeratum/memento/archive/0.1.tar.gz",
+    keywords=['data', 'python'],
     packages=["memento"],
 )
